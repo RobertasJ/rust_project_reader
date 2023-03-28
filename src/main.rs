645f8a53d3d9f9c283afaa9560b63c71bffa64a4
_Robertas <robertasj8@gmail.com>
2023-03-28 10:59:35 +0200

    added support for more languages

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use walkdir::{DirEntry, WalkDir};
 use clipboard::{ClipboardProvider, ClipboardContext};
 use colored::*;
 
-fn is_not_target(entry: &DirEntry) -> bool {
+fn blacklist(entry: &DirEntry) -> bool {
     !entry
         .path()
         .components()
-        .any(|c| c.as_os_str() == "target")
+        .any(|c| c.as_os_str() == "target" || c.as_os_str() == "node_modules")
 }
 
 fn main() -> io::Result<()> {
@@ -21,16 +21,32 @@ fn main() -> io::Result<()> {
 
     for entry in WalkDir::new(path)
         .into_iter()
-        .filter_entry(|e| is_not_target(e))
+        .filter_entry(|e| blacklist(e))
         .filter_map(|e| e.ok())
     {
-        if entry.file_type().is_file() && entry.path().extension().map_or(false, |ext| ext == "rs") {
+        if entry.file_type().is_file() {
+            let ext = entry.path().extension().unwrap().to_str().unwrap(); 
+            let ext = match ext {
+                "rs" | "json" => {
+                    ext
+                },
+                "svelte" => {
+                    "html"
+                },
+                "js" => {
+                    "javascript"
+                },
+                "ts" => {
+                    "typescript"
+                },
+                _ => continue,
+            };
             let mut file = File::open(entry.path())?;
             let mut contents = String::new();
             file.read_to_string(&mut contents)?;
 
             output.push_str(&format!("{}", entry.path().display()));
-            output.push_str("\n```rust\n");
+            output.push_str(&format!("\n```{ext}\n"));
             output.push_str(&contents);
             output.push_str("```\n\n");
         }
